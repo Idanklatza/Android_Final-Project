66db8e3daa9286e0aedd3ee8e430b67463338b39
Idan klatza <93584464+Idanklatza@users.noreply.github.com>
2023-01-12 13:02:56 +0200

    User Model

---
+++ app/src/main/java/com/example/travelapp/model/User.java
@@ -0,0 +1,38 @@
+package com.example.travelapp.model;
+
+public class User {
+
+    String name;
+    String password;
+    String img;
+
+    public User(String name, String password, String img) {
+        this.name = name;
+        this.password = password;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
